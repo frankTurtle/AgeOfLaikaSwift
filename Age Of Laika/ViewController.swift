1315fde242cc16bb2fa32d7c314fc6ed3de21802
Barret J. Nobel <bear.nobel@gmail.com>
2016-05-24 18:08:40 -0400

    wrote implementation of actualDogYears button updates label, unhides, hides keyboard as well

--- Age Of Laika/ViewController.swift
+++ Age Of Laika/ViewController.swift
@@ -37,6 +37,18 @@ class ViewController: UIViewController {
     }
 
     @IBAction func convertToDogYearsActualPressed(sender: UIButton) {
+        let stringFromText = enteredHumanYearsTextField.text! //........................... gets data from TF
+        let convertedAge = Double( stringFromText )! //.................................... converts to double
+        
+        var realDogYears: Double
+        
+        if( convertedAge > 2 ){ realDogYears = ( 10.5 * 2 ) + ( convertedAge - 2 ) * 4 } //. actual math
+        else{ realDogYears = convertedAge * 10.5 }
+        
+        dogYearsLabel.text = "\(realDogYears) Human Years" //............................... update labels and unhide
+        dogYearsLabel.hidden = false
+        
+        enteredHumanYearsTextField.resignFirstResponder() //................................ hide keyboard
     }
 }
 
